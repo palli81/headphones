146dcd1808522e7b4bb3e29b21843c256641d2fd
Pall Valmundsson <pall.valmundsson@gmail.com>
2011-11-08 21:59:53 +0000

    SQDD: Template fix, checkbox wasn't saved to config.ini

--- headphones/webserve.py
+++ headphones/webserve.py
@@ -390,7 +390,7 @@ class WebInterface(object):
 		usenet_retention=None, nzbmatrix=0, nzbmatrix_username=None, nzbmatrix_apikey=None, newznab=0, newznab_host=None, newznab_apikey=None,
 		nzbsorg=0, nzbsorg_uid=None, nzbsorg_hash=None, newzbin=0, newzbin_uid=None, newzbin_password=None, preferred_quality=0, preferred_bitrate=None, detect_bitrate=0, move_files=0, 
 		torrentblackhole_dir=None, download_torrent_dir=None, numberofseeders=10, use_isohunt=0, use_kat=0, use_mininova=0, 
-		rename_files=0, correct_metadata=0, cleanup_files=0, add_album_art=0, embed_album_art=0, embed_lyrics=0, destination_dir=None, separate_quality_destination_dirs=False, lossy_destination_dir=None, lossless_destination_dir=None, folder_format=None, file_format=None, include_extras=0, interface=None, log_dir=None,
+		rename_files=0, correct_metadata=0, cleanup_files=0, add_album_art=0, embed_album_art=0, embed_lyrics=0, destination_dir=None, separate_quality_destination_dirs=0, lossy_destination_dir=None, lossless_destination_dir=None, folder_format=None, file_format=None, include_extras=0, interface=None, log_dir=None,
 		encode=0, encoder=None, bitrate=None, samplingfrequency=None, encoderfolder=None, advancedencoder=None, encoderoutputformat=None, encodervbrcbr=None, encoderquality=None, encoderlossless=0):
 
 		headphones.HTTP_HOST = http_host
